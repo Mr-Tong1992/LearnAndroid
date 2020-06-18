23ccff0e6ff597019a2cd3c7c35f58d9596d460a
601725 <qiang@syswin.com>
2020-06-18 09:27:26 +0800

    初始化

---
+++ MyApplication3/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+//import android.support.v4.app.NavUtils;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button  button;
+        button = (Button)findViewById(R.id.but1);
+//        button
+//        System.out.println(button);
+
+
+
+
+
+    }
+
+//    public boolean onCreateOptionsMenu(Menu menu){
+//        getMenuInflater().inflate(R.menu.activity_main,menu);
+//        return true;
+//    }
+}
